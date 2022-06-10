0c258699f2b340258533bca770d558c36cf99a3e
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-10 22:12:52 +0000

    feature-3 completed

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const ClientError = require('./client-error');
 require('dotenv/config');
 const path = require('path');
 const express = require('express');
+const app = express();
 const errorMiddleware = require('./error-middleware');
+const argon2 = require('argon2');
 const pg = require('pg');
+const jsonMiddleware = express.json();
+const publicPath = path.join(__dirname, 'public');
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -11,18 +15,13 @@ const db = new pg.Pool({
   }
 });
 
-const app = express();
-const publicPath = path.join(__dirname, 'public');
+app.use(jsonMiddleware);
 
 if (process.env.NODE_ENV === 'development') {
   app.use(require('./dev-middleware')(publicPath));
 }
 
 app.use(express.static(publicPath));
-
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
-});
 
 app.get('/api/users', (req, res, next) => {
   const sql = `
@@ -67,6 +66,40 @@ app.get('/api/users/:userId', (req, res, next) => {
       res.json(queryResult.rows[0]);
     })
     .catch(err => next(err));
+});
+
+app.post('/api/auth/sign-up', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+
+  argon2.hash(password)
+    .then(hashResult => {
+      const sql = `
+        insert into "users" ("username", "hashedPassword")
+        values($1, $2)
+        returning *;
+      `;
+      const params = [username, hashResult];
+      db.query(sql, params)
+        .then(queryResult => {
+          const [newUser] = queryResult.rows;
+          res.status(201).json(newUser);
+        })
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({
+            error: 'a happy accident occurred'
+          });
+        });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'a happy accident occurred'
+      });
+    });
 });
 
 app.use(errorMiddleware);
