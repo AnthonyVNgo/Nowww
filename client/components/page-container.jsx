8d2b0aedf22cf4e3d40f48380426a870739fbcc7
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-07-31 06:28:49 +0000

    final-project-ui user details component built for reuse need to update the updatenowpage and minor css for list items

--- client/components/page-container.jsx
+++ client/components/page-container.jsx
@@ -8,7 +8,7 @@ const styles = {
 
 export default function PageContainer({ children }) {
   return (
-    <div className="bg-light pt-3 pb-4">
+    <div className="bg-light pt-md-3 pb-md-4">
       <div className="container" style={styles.page}>
         {children}
       </div>
