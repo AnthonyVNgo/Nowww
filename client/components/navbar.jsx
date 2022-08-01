a70448dbc9180be32f89a6a8321f63d2f4b4429b
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-08-01 08:02:03 +0000

    final-project-ui more css style touchups offcanvas menu wip

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -12,7 +12,7 @@ export default class Navbar extends React.Component {
           </a>
           <div>
 
-            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            <button className="btn btn-link" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
               </svg>
@@ -25,7 +25,7 @@ export default class Navbar extends React.Component {
               </div>
               <div className="offcanvas-body">
                 {user !== null &&
-                  <ul>
+                  <ul className='off-canvas-items'>
                     <li><a href="#my-now">My Nowww</a></li>
                     <li><a href="#edit">Edit Nowww</a></li>
                     <li><a href="#">Nowww Pages</a></li>
@@ -33,7 +33,7 @@ export default class Navbar extends React.Component {
                   </ul>
                 }
                 {user === null &&
-                  <ul>
+                  <ul className='off-canvas-items'>
                     <li><a href="#">Nowww Pages</a></li>
                     <li><a href="#sign-up">Sign-up</a></li>
                     <li><a href="#sign-in">Login</a></li>
@@ -41,41 +41,6 @@ export default class Navbar extends React.Component {
                 }
               </div>
             </div>
-
-            {/* {user !== null &&
-            <div>
-              <div className="dropdown">
-                <button className="btn" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                  </svg>
-                </button>
-                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-                  <li><a className="dropdown-item" href="#my-now">My Nowww</a></li>
-                  <li><a className="dropdown-item" href="#edit">Edit Nowww</a></li>
-                  <li><a className="dropdown-item" href="#">Nowww Pages</a></li>
-                  <li><a className="dropdown-item" href="#" onClick={handleSignOut}>Sign-out</a></li>
-                </ul>
-              </div>
-            </div>
-            }
-
-            {user === null &&
-            <div>
-              <div className="dropdown">
-                <button className="btn" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                  </svg>
-                </button>
-                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-                  <li><a className="dropdown-item" href="#">Nowww Pages</a></li>
-                  <li><a className="dropdown-item" href="#sign-up">Sign-up</a></li>
-                  <li><a className="dropdown-item" href="#sign-in">Login</a></li>
-                </ul>
-              </div>
-            </div>
-            } */}
           </div>
         </div>
       </nav>
