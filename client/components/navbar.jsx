5bc256981951729bf9bfd6738e94fa6068582ce1
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-08-01 07:32:57 +0000

    final-project-ui css improvements

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -11,7 +11,38 @@ export default class Navbar extends React.Component {
             Nowww
           </a>
           <div>
-            {user !== null &&
+
+            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+              </svg>
+            </button>
+
+            <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasRightLabel">Nowww</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div className="offcanvas-body">
+                {user !== null &&
+                  <ul>
+                    <li><a href="#my-now">My Nowww</a></li>
+                    <li><a href="#edit">Edit Nowww</a></li>
+                    <li><a href="#">Nowww Pages</a></li>
+                    <li><a href="#" onClick={handleSignOut}>Sign-out</a></li>
+                  </ul>
+                }
+                {user === null &&
+                  <ul>
+                    <li><a href="#">Nowww Pages</a></li>
+                    <li><a href="#sign-up">Sign-up</a></li>
+                    <li><a href="#sign-in">Login</a></li>
+                  </ul>
+                }
+              </div>
+            </div>
+
+            {/* {user !== null &&
             <div>
               <div className="dropdown">
                 <button className="btn" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
@@ -44,7 +75,7 @@ export default class Navbar extends React.Component {
                 </ul>
               </div>
             </div>
-            }
+            } */}
           </div>
         </div>
       </nav>
