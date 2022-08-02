9a2b5ab85e94cabfb236e488ac94f54e07a36c1e
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-08-02 07:23:08 +0000

    loading spinner wip roadblocks with offcanvas functionality

---
+++ client/components/loading.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// export default function Loading() {
+//   return(
+//     <div>
+//       100vh
+//       100vw
+//       jc-center
+//       align-items center
+//       loading gif
+//       LOGO above loading gif
+//       css styling
+//     </div>
+//   )
+// }
